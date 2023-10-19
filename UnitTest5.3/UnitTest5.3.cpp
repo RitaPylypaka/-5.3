acb6d743a22e4c7ba03681b7bccf871dcb45a175
RitaPylypaka <ritapylypaka@gmail.com>
2023-10-19 12:53:49 +0300

    Add project files.

---
+++ UnitTest5.3/UnitTest5.3.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../пр5.3/пр5.3.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest53
+{
+	TEST_CLASS(UnitTest53)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double m;
+			m = p(1);
+			Assert::AreEqual(m, -0.236895, 0.000001);
+		}
+	};
+}
---
+++ UnitTest5.33/UnitTest5.33.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../пр5.3/пр5.3.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest533
+{
+	TEST_CLASS(UnitTest533)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double t;
+			t = p(1);
+			Assert::AreEqual(t,-0.236895 , 0.000001);
+		}
+	};
+}
